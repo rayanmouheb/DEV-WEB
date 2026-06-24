1c838933215daf04e1b328b1eba32efd16c14dfd
jmk04 <jkoffi506@gmail.com>
2026-06-24 23:39:23 +0200

    cors fix

--- Back-entreprise/src/index.js
+++ Back-entreprise/src/index.js
@@ -6,7 +6,7 @@ import router from './routes/index.js'
 const app = express()
 const PORT = process.env.PORT ?? 3000
 
-app.use(cors({ origin: process.env.FRONTEND_URL ?? 'http://localhost:5173' }))
+app.use(cors({ origin: /^http:\/\/localhost(:\d+)?$/ }))
 app.use(express.json())
 app.use('/api', router)
 
